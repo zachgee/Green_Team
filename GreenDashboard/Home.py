b2d5cfe62ddb9f99f634fb06d49276592d52f3f9
unknown <zgonzales16@gmail.com>
2022-01-24 21:03:07 -0600

    Home Dashboard

--- GreenDashboard/Home.py
+++ GreenDashboard/Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PIL import Image
+import os
 
 introduction_1 = "After the devastating winter freeze last year, Texas energy use & distribution was put under a microscope. Texas produces and consumes more electricity than any other state; in fact, it's the only state that runs a stand-alone independent electricity grid. Therefore, Texans demanded to know why the power grid suddenly failed us in such a dire time & what is being done to prevent future tragedies.\n "
 
 introduction_2 = "\n To determine what can be changed regarding our energy structure, we've compiled some data sets from the City of Austin. While analyzing this subset of the Texas power grid, we're hoping to answer: what is the current consumer demand & has it increased over the last few years? What energy sources are we using? What are green alternatives in the capital and how are those being developed and/or utilized?"
 
-
-
-ImageWelcome = "C:\\Users\joelf\Documents\Bootcamp\Data_Analytics\Project\GreenDashboard\Resources\Images/Austin_Back_carlos-alfonso.jpg"
+dir_test = os.getcwd()
+ImageWelcome = dir_test + "\Resources\Images/Austin_Back_carlos-alfonso.jpg"
 background = Image.open(ImageWelcome)
 
 with st.container():

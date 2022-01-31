41defa98c1c462f2963cc81b5430391f4479d387
unknown <zgonzales16@gmail.com>
2022-01-31 14:29:01 -0600

    Power Plants screen in progress

--- GreenDashboard/ES_PowerPlants_Prod.py
+++ GreenDashboard/ES_PowerPlants_Prod.py
@@ -24,6 +24,8 @@ connection = psycopg2.connect(host=DB_HOST,user=DB_USER,password=DB_PASS,databas
 
 cursor =  connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+option = st.sidebar.selectbox("Which Dashboard?", ('Power Supply Cost', 'Power Plants',), )
+
 query = ('SELECT public."EO_PowerSupply_Cost"."Fuel_Type", public."EO_PowerSupply_Cost"."Fiscal_Year", public."EO_PowerSupply_Cost"."Fuel_Cost", public."EO_PowerSupply_Cost"."Fuel_Cost_Per"' +
         'FROM public."EO_PowerSupply_Cost"')
 
@@ -31,7 +33,9 @@ cursor.execute(query)
 result = cursor.fetchall()
 connection.close()
 df = pd.DataFrame(result) 
+df.rename(columns={0: 'Fuel Type',1:'Fiscal Year',2:'Fuel Cost',3:'Fuel Cost Percentage'}, inplace=True)
 st.dataframe(df )
+st.line_chart(df)
 
 
 

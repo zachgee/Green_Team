6b19440e245a37fe46e839226e8c2cf1bd50c30d
unknown <zgonzales16@gmail.com>
2022-02-01 22:49:35 -0600

    adding table to page

--- GreenDashboard_v4/app/Screens/ES/ES_Renewable_Res.py
+++ GreenDashboard_v4/app/Screens/ES/ES_Renewable_Res.py
@@ -2,30 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import psycopg2, psycopg2.extras
+import sys 
+import os
+
+dir_sys = os.path.join(os.getcwd(), "app", "Resources","Animations")
+sys.path.insert(0, dir_sys)
+import Animations
+
+dir_sys = os.path.join(os.getcwd(), "app", "Resources","Functions")
+sys.path.insert(0, dir_sys)
+import GT_Functions as gt_func
+
+
+vSQL = 'Select * from public."EO_Fuel_Cost_AVG"'
+df = gt_func.Execute_query(vSQL)
+df.rename(columns={0: 'Fiscal Year',1:'System annual average fuel cost (cents/kWh)'}, inplace=True)
 
 st.title('Renewable Resources')
 
 st.subheader('Dataset sample')
 
-DB_HOST = 'dataviz.cgq2ewzuuqs1.us-east-2.rds.amazonaws.com'
-DB_USER = 'postgres'
-DB_PASS = 'ElPeruano_2021'
-DB_NAME = 'postgres'
-DB_PORT = 5432
-connection = psycopg2.connect(host=DB_HOST,user=DB_USER,password=DB_PASS,database=DB_NAME, port=DB_PORT)
-
-cursor =  connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-vSQL = 'Select * from public."EO_Fuel_Cost_AVG"'
-
-cursor.execute(vSQL)
-result = cursor.fetchall()
-
-df = pd.DataFrame(result)
-df.rename(columns={0: 'Fiscal Year',1:'System annual average fuel cost (cents/kWh)'}, inplace=True)
-
-query = vSQL
-
-cursor.execute(query)
-result = cursor.fetchall()
 st.dataframe(df)
